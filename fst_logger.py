4b9cf3160012438e71940bc1e2e91a09cc6cb808
Сема <belyay_kin@bk.ru>
2026-04-05 13:52:34 +0300

    Добавил параматезированный декоратор и отредактированный декоратор

--- fst_logger.py
+++ fst_logger.py
@@ -11,11 +11,11 @@ def logger(old_function):
         func_name = old_function.__name__
         args = args
         kwargs = kwargs
-        res = (f'Название функции: {func_name}\n'
-                f'🗓️Дата и время вызова: {date_func_call}\n'
-                f'Аргументы поцизионные: {args if args else 'нет таких'}\n' 
-                f'Аргументы именованные: {kwargs if kwargs else 'нет таких'}\n'
-                f'Результат выполнения: {result}\n\n\n')
+        res = (f"Название функции: {func_name}\n"
+                f"🗓️Дата и время вызова: {date_func_call}\n"
+                f"Аргументы поцизионные: {args if args else 'нет таких'}\n" 
+                f"Аргументы именованные: {kwargs if kwargs else 'нет таких'}\n"
+                f"Результат выполнения: {result}\n\n\n")
 
         with open('main.log', 'a') as f:
             f.write(res)
@@ -25,43 +25,43 @@ def logger(old_function):
     return new_function
 
 
-# def test_1():
+def test_1():
 
-#     path = 'main.log'
-#     if os.path.exists(path):
-#         os.remove(path)
+    path = 'main.log'
+    if os.path.exists(path):
+        os.remove(path)
 
-#     @logger
-#     def hello_world():
-#         return 'Hello World'
+    @logger
+    def hello_world():
+        return 'Hello World'
 
-#     @logger
-#     def summator(a, b=0):
-#         return a + b
+    @logger
+    def summator(a, b=0):
+        return a + b
 
-#     @logger
-#     def div(a, b):
-#         return a / b
+    @logger
+    def div(a, b):
+        return a / b
 
-#     assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
-#     result = summator(2, 2)
-#     assert isinstance(result, int), 'Должно вернуться целое число'
-#     assert result == 4, '2 + 2 = 4'
-#     result = div(6, 2)
-#     assert result == 3, '6 / 2 = 3'
+    assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+    result = summator(2, 2)
+    assert isinstance(result, int), 'Должно вернуться целое число'
+    assert result == 4, '2 + 2 = 4'
+    result = div(6, 2)
+    assert result == 3, '6 / 2 = 3'
     
-#     assert os.path.exists(path), 'файл main.log должен существовать'
+    assert os.path.exists(path), 'файл main.log должен существовать'
 
-#     summator(4.3, b=2.2)
-#     summator(a=0, b=0)
+    summator(4.3, b=2.2)
+    summator(a=0, b=0)
 
-#     with open(path) as log_file:
-#         log_file_content = log_file.read()
+    with open(path) as log_file:
+        log_file_content = log_file.read()
 
-#     assert 'summator' in log_file_content, 'должно записаться имя функции'
-#     for item in (4.3, 2.2, 6.5):
-#         assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+    assert 'summator' in log_file_content, 'должно записаться имя функции'
+    for item in (4.3, 2.2, 6.5):
+        assert str(item) in log_file_content, f'{item} должен быть записан в файл'
 
 
-# if __name__ == '__main__':
-#     test_1()
+if __name__ == '__main__':
+    test_1()
---
+++ параматезированный_декоратор.py
@@ -0,0 +1,66 @@
+import os
+from functools import wraps
+from datetime import datetime
+
+def logger(path):
+    def __logger(old_function):
+        @wraps(old_function)
+        def new_function(*args, **kwargs):
+            result = old_function(*args, **kwargs)
+            res = (f'Название функции: {old_function.__name__}\n'
+                   f'Дата и время вызова функции: {datetime.now()}\n'
+                   f'Аргументы функции: {args}, {kwargs}\n'
+                   f'Результат: {result}\n\n\n')
+            
+            with open(path, 'a') as f:
+                f.write(res)
+
+            return(result)
+
+        return new_function
+
+    return __logger
+
+
+def test_2():
+    paths = ('log_1.log', 'log_2.log', 'log_3.log')
+
+    for path in paths:
+        if os.path.exists(path):
+            os.remove(path)
+
+        @logger(path)
+        def hello_world():
+            return 'Hello World'
+
+        @logger(path)
+        def summator(a, b=0):
+            return a + b
+
+        @logger(path)
+        def div(a, b):
+            return a / b
+
+        assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+        result = summator(2, 2)
+        assert isinstance(result, int), 'Должно вернуться целое число'
+        assert result == 4, '2 + 2 = 4'
+        result = div(6, 2)
+        assert result == 3, '6 / 2 = 3'
+        summator(4.3, b=2.2)
+
+    for path in paths:
+
+        assert os.path.exists(path), f'файл {path} должен существовать'
+
+        with open(path) as log_file:
+            log_file_content = log_file.read()
+
+        assert 'summator' in log_file_content, 'должно записаться имя функции'
+
+        for item in (4.3, 2.2, 6.5):
+            assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_2()
